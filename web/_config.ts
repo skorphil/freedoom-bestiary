50464c8022d3c6c2bbdab6b96cfee79409c11b7f
Philipp Rich <skorphil@gmail.com>
2026-06-21 23:24:52 +0400

    ci: fix css loading for github subdirectory

--- web/_config.ts
+++ web/_config.ts
@@ -2,8 +2,8 @@ import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
 
 const site = lume({
-  src: "./",
-  dest: "./_site",
+	src: "./",
+	dest: "./_site",
 });
 site.use(jsx());
 site.add("styles.css");
--- web/_includes/main.tsx
+++ web/_includes/main.tsx
@@ -1,20 +1,19 @@
 export default ({ title, children }: Lume.Data) => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title || "Freedoom Bestiary"}</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <main>
-          {children}
-        </main>
-        <footer>
-          <a href="https://github.com/skorphil/freedoom-bestiary">Github</a>
-        </footer>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<base href="/freedoom-bestiary/" />
+				<title>{title || "Freedoom Bestiary"}</title>
+				<link rel="stylesheet" href="/styles.css" />
+			</head>
+			<body>
+				<main>{children}</main>
+				<footer>
+					<a href="https://github.com/skorphil/freedoom-bestiary">Github</a>
+				</footer>
+			</body>
+		</html>
+	);
 };
